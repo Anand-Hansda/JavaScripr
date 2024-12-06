2ff41f784623137cca776294855342effb999dc4
Anand-Hansda <anandhansda0007@gmail.com>
2024-12-06 15:35:40 +0000

    datatype updated

---
+++ 02_dataTypes.js
@@ -0,0 +1,11 @@
+"use strict"; //treat all JS code as new version 
+
+let name = "Anand"   // string
+let age = 25        // number
+let isLogedIn = true  // boolen
+let state;  //undefined
+
+console.log(typeof undefined)
+
+
+
---
+++ dataTypesConversion.js
@@ -0,0 +1,17 @@
+let score = "33aa"
+
+let scoreNumber = Number(score)
+
+// let scoreString = score(toString)
+
+console.log(typeof(score))
+
+/*
+
+"33" => 33
+"33adas" => NaN
+true & false => 0 - 1
+"" => false
+"jhvfj" => true
+
+*/
