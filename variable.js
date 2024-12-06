bcdfde22af27cced88e312b8bfee667f8a899677
Anand-Hansda <anandhansda0007@gmail.com>
2024-12-06 15:30:59 +0000

    updated

---
+++ variable.js
@@ -0,0 +1,18 @@
+const accountId = 65456
+let accountEmail = "anandkh053@gmail.com"
+var accountPassword = "1548"
+accountCity = "Dhanbaad"
+let accountState;
+
+
+
+console.log(additation)
+
+console.log([accountId, accountEmail, accountPassword, accountCity])
+console.log(accountState)
+/*
+
+prefer not to use the "var"!
+
+
+*/
