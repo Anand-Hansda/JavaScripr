e85f7ab754ef632e729ba02a458497d17f72ae5c
Anand-Hansda <anandhansda002@gmail.com>
2024-12-07 21:32:59 +0530

    num&mat

---
+++ num&maths.js
@@ -0,0 +1,40 @@
+// let score = 400
+// console.log(score)
+
+
+
+// const balance = new Number(100)
+// console.log(balance)
+
+
+
+// console.log(balance.toString().length)
+// console.log(balance.toFixed(2))
+
+
+// const otherNumber = 125.8588
+// console.log(otherNumber.toPrecision(5))
+
+// const hunderds = 1578755454
+// console.log(hunderds.toLocaleString('en'))
+
+
+
+// *************************** MATHS *****************************
+
+// console.log(Math)
+// console.log(Math.abs(-4))
+// console.log(Math.round(4.43));
+// console.log(Math.ceil(4.43));
+// console.log(Math.floor(4.53));
+// console.log(Math.min(8,6,5,7,9,2))
+// console.log(Math.max(8,6,5,7,9,2))
+
+console.log((Math.random()*10) + 1)
+
+
+const min = 10
+const max = 20
+
+
+
