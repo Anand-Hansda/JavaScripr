19bd0fa6007a78c9a8b9ad8906aab14a877c96bd
Anand-Hansda <anandhansda002@gmail.com>
2024-12-12 22:59:26 +0530

    updated

--- objects.js
+++ objects.js
@@ -19,20 +19,42 @@ const regularUser = {
 // console.log(regularUser);
 
 
-const obj1 = {1: "a", 2: "b"}
-const obj2 = {1: "a", 2: "b"}
+// const obj1 = {1: "a", 2: "b"}
+// const obj2 = {1: "a", 2: "b"}
 
 // const obj3 = Object.assign({}, obj1, obj2)
-const obj3 = {...obj1, ...obj2}
+// const obj3 = {...obj1, ...obj2}
 
 // console.log(obj3);
 
 
 
-console.log(`Hay my name is ${regularUser.fullname.userfullname.firstname} ${regularUser.fullname.userfullname.lastname} and my email is ${regularUser.email}`);
+// console.log(`Hay my name is ${regularUser.fullname.userfullname.firstname} ${regularUser.fullname.userfullname.lastname} and my email is ${regularUser.email}`);
 
-console.log(tinderUser)
+// console.log(tinderUser)
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+
+
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "Anand"
+
+}
+
+
+const {courseInstructor} = course
+console.log(courseInstructor);
+
+
+// const navbar = ({company}) => {
+
+// }
+// navbar(company = "Anand")
+// console.log(navbar);
+
+
 
